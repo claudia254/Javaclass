0363109ee1921e91147225303d2b9b22d907231c
claudia-netizen <your-email@example.com>
2024-10-22 10:46:29 +0300

    first commit

---
+++ assets/js/js basic.js
@@ -0,0 +1,7 @@
+// get the button element
+const javaclass:HTMLElement=document.getElementById(elementid : colorbutton)
+// add event listener for the button
+javaclass.addEventListener('click',()=>{
+   //  change the color of the button
+   javaclass.style.backgroundColor=javaclass.style.backgroundColor==='blue' ? 'blue' :'blue'
+});
